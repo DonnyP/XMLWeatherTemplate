30b02dbc32dcfe9d6e3d5ce3ad2b95d2e90e969b
DonnyP <DonnyPhim@gmail.com>
2016-04-29 10:07:28 -0400

    Changed the background

--- XMLWeather/loadScreen.Designer.cs
+++ XMLWeather/loadScreen.Designer.cs
@@ -124,7 +124,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Red;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.Controls.Add(this.day2Clouds);
             this.Controls.Add(this.day1Clouds);
             this.Controls.Add(this.min2Output);
